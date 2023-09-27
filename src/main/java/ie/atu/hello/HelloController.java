4d3c980a313df53c13f48fe9f7b348b72ef0a1d9
Johnson <G00389668@atu.ie>
2023-09-27 10:54:25 +0100

    Added getName to HelloController

--- src/main/java/ie/atu/hello/HelloController.java
+++ src/main/java/ie/atu/hello/HelloController.java
@@ -1,6 +1,7 @@
 package ie.atu.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "Hello World";
+    }
+
+    @GetMapping("/greet/{name}")
+    public String getName(@PathVariable String name){
+        return "Hello " + name;
     }
 }
